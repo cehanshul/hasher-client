fe051082abe7dc4612b17aa53eedf871ce8ea348
cehanshul <cehanshul@gmail.com>
2024-06-23 23:58:56 +0530

    adding new download button

--- app/[username]/ExpertProfilePage.tsx
+++ app/[username]/ExpertProfilePage.tsx
@@ -547,8 +547,27 @@ const ExpertProfilePage = ({
                     // onClick={showCheckoutDetailsSection}
                     onClick={handleClick}
                   >
-                    <p className="text-center mx-auto text-xl md:text-2xl py-2 md:py-3 text-white">
+                    {/* <p className="text-center mx-auto text-xl md:text-2xl py-2 md:py-3 text-white">
                       Download The app
+                    </p> */}
+                    <div className="flex gap-2">
+                      <Image
+                        className="my-auto justify-center"
+                        src="/images/icons/calender.svg"
+                        alt="user"
+                        height={44}
+                        width={44}
+                      />
+                      <div className="text-start">
+                        <p className="text-[#ffffff] text-lg">{duration} min</p>
+                        <p className="text-md">Video Session</p>
+                      </div>
+                    </div>
+                    <p className="px-3 py-2 flex gap-1 items-center font-semibold text-lg rounded-full bg-white">
+                      Download
+                      {/* <span>
+                        <FiArrowRight size={24} className="font-bold" />
+                      </span> */}
                     </p>
                     {/* <div className="flex gap-2">
                       <Image
